2160c0813fc1750e9d253d1ae440bd6f1cbbc215
Naireen <naireen.hussain222@live.com>
2018-03-26 19:58:40 +0000

    more functionallity with some error handling

--- Arduino/comm.py
+++ Arduino/comm.py
@@ -3,7 +3,11 @@
 import serial , string, time
 
 output = " " 
-ser = serial.Serial('/dev/ttyACM0', 9600, 8, 'N', 1, timeout=1)
+#need to account for fact that the comport can change
+try:
+    ser = serial.Serial('/dev/ttyACM0', 9600, 8, 'N', 1, timeout=1)
+except:
+    ser = serial.Serial('/dev/ttyACM1', 9600, 8, 'N', 1, timeout=1)
 counter = 0 
 temp, humid = 0, 0
 
@@ -15,13 +19,28 @@ while counter < 10:
     #parse the data
     # output will terminate after a newline char from arduino
     # newline char in arduino is given by "\r" 
-    val = float(output.split("= ")[1].split("\r")[0])
+    bug = True
+    val = ""
+    while bug:
+        try:
+            val = float(output.split("= ")[1].split("\r")[0])
+            bug =  False
+        except:
+            # keep appending to val
+            temp = ser.readline()
+            print("Old val", val)
+            val = val + temp
+            print("Temp", temp)
+
     if output[0] == "T" : #its a temp
         temp = val
     elif output[0] == "H": # its humidity
         humid = val
         
     print "Temp", temp, "Humidity", humid
-
+    count = str(counter %3)
+    ser.write(count)
+    time.sleep(3)
     output = " "
     counter +=1
+#ser.write('3')
