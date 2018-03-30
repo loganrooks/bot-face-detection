8fc3163f2870adb7a6791366c024f98039a06428
Said Chehabeddine <saeed.chehabeddine@gmail.com>
2018-03-30 12:53:41 -0400

    serial test

--- SerialCom/serialArduino/serialArduino.ino
+++ SerialCom/serialArduino/serialArduino.ino
@@ -4,23 +4,23 @@ int a =0;
 void setup() {
 Serial.begin(9600);              //Starting serial communication
 }
-  
+ /*
 void loop() {
   a++;                          // a value increase every loop
   sprintf(dataString,"%02X",a); // convert a value to hexa 
   Serial.println(dataString);   // send the data
   delay(1000);                  // give the loop some break
 }
-
+*/
 void loop(){
 
 	if(Serial.available() > 0){
-		incomingByte = Serial.read();
+		char incomingByte = Serial.read();
 		switch((char)incomingByte){
-		 case '0':
+		 case 0:
 			Serial.println("uh-uh");
 		  break;
-		 case '1':
+		 case 1:
 			Serial.println("yep");
 		  break;
 
