9b5243f06668dcd79cc8f97ab6af1e90d869ea65
loganrooks <logansrooks@gmail.com>
2018-03-30 15:43:23 -0400

    added viola jones algorithm to faces.py and main.py

--- main.py
+++ main.py
@@ -1,29 +1,41 @@
 import cv2
+import time
 import picamera
+import imutils
 from picamera.array import PiRGBArray
+import faces
 from face_client import FaceClient
-from faces import *
+import json
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-c", "--conf", default="./conf.json", help="path to JSON config file")
+args = vars(parser.parse_args())
+
+conf = json.load(open(args["conf"]))
 
 API_KEY = 'egk0ko9neatt4ftsgmhsmklf2r'
 API_SECRET = '67doedp6s4dcnm5kmuhkgetj8p'
 client = FaceClient(API_KEY, API_SECRET)
 metrics = ['gender', 'mood', 'smiling', 'eyes']
+
 domain_name = 'loganrookspi.ddns.net'
 filename = "/var/www/html/temp.jpg"
 img_addr = 'http://{}/temp.jpg'.format(domain_name)
 
-resolution = (640, 480)
 
 camera = picamera.PiCamera()
-camera.resolution = resolution
-camera.framerate = 16
-rawCapture = PiRGBArray(camera, size=resolution)
+camera.resolution = conf['resolution']
+camera.framerate = conf['frame_rate']
+rawCapture = PiRGBArray(camera, size=conf['resolution'])
 time.sleep(2)
 
-for f in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True, ):
+for f in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
     frame = f.array
     frame = imutils.resize(frame, width=400)
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    descriptions = recognize_faces(img_addr, client, metrics)
-
+    is_face, face = faces.viola_jones(frame, conf["scale_factor"], conf["min_neighbors"], conf["min_size"], conf["size_thresh"])
+    if is_face:
+        cv2.imwrite(filename, face)
+        descriptions = faces.recognize_faces(img_addr, client, metrics)
+        print descriptions
