f45fd87cf402e6d4f22ed906212d39930f6d9a22
loganrooks <logansrooks@gmail.com>
2018-03-30 14:36:33 -0400

    updated main.py

--- faces.py
+++ faces.py
@@ -9,11 +9,6 @@ import time
 import imutils
 from face_client import FaceClient
 
-API_KEY = 'egk0ko9neatt4ftsgmhsmklf2r'
-API_SECRET = '67doedp6s4dcnm5kmuhkgetj8p'
-client = FaceClient(API_KEY, API_SECRET)
-metrics = ['gender', 'mood', 'smiling', 'eyes']
-
 def extract_identity(uids, threshold):
     max_confidence = 0
     identity = None
@@ -50,29 +45,6 @@ def recognize_faces(img, client, metrics):
             face_descriptions.append(description)
     return face_descriptions
 
-
-domain_name = 'loganrookspi.ddns.net'
-filename = "/var/www/html/temp.jpg"
-client = FaceClient(API_KEY, API_SECRET)
-img_addr = 'http://{}/temp.jpg'.format(domain_name)
-
-resolution = (640, 480)
-
-camera = picamera.PiCamera()
-camera.resolution = resolution
-camera.framerate = 16
-rawCapture = PiRGBArray(camera, size=resolution)
-time.sleep(2)
-
-for f in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True,):
-    frame = f.array
-    frame = imutils.resize(frame, width=400)
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    
-
-    descriptions = recognize_faces(img_addr, client, metrics)
-
-print descriptions
 
 
 
--- main.py
+++ main.py
@@ -1,9 +1,29 @@
-import pickle
+import cv2
+import picamera
+from picamera.array import PiRGBArray
+from face_client import FaceClient
+from faces import *
 
-import video_track
+API_KEY = 'egk0ko9neatt4ftsgmhsmklf2r'
+API_SECRET = '67doedp6s4dcnm5kmuhkgetj8p'
+client = FaceClient(API_KEY, API_SECRET)
+metrics = ['gender', 'mood', 'smiling', 'eyes']
+domain_name = 'loganrookspi.ddns.net'
+filename = "/var/www/html/temp.jpg"
+img_addr = 'http://{}/temp.jpg'.format(domain_name)
 
-print("Hello World!")
+resolution = (640, 480)
 
-message = 'Hello World!'
-#with open("/home/pi/Documents/rock-bot/test.txt", 'wb') as f:
-#    pickle.dump(message, f, protocol=pickle.HIGHEST_PROTOCOL)
+camera = picamera.PiCamera()
+camera.resolution = resolution
+camera.framerate = 16
+rawCapture = PiRGBArray(camera, size=resolution)
+time.sleep(2)
+
+for f in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True, ):
+    frame = f.array
+    frame = imutils.resize(frame, width=400)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    descriptions = recognize_faces(img_addr, client, metrics)
+
