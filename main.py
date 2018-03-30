a77eb1cdc252f37ead14f10fa687aacca205a789
Naireen <naireen.hussain222@live.com>
2018-03-30 14:17:26 -0400

    main

---
+++ main.py
@@ -0,0 +1,9 @@
+import pickle
+
+import video_track
+
+print("Hello World!")
+
+message = 'Hello World!'
+#with open("/home/pi/Documents/rock-bot/test.txt", 'wb') as f:
+#    pickle.dump(message, f, protocol=pickle.HIGHEST_PROTOCOL)
